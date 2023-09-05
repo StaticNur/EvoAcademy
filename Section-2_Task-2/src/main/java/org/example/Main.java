f134c1c18bb71f35eeb1624950e185eba467d506
Mukhammadnur <gsjxne@gmail.com>
2023-09-05 21:54:44 +0300

    "ready fix"

--- Section-2_Task-2/src/main/java/org/example/Main.java
+++ Section-2_Task-2/src/main/java/org/example/Main.java
@@ -10,9 +10,12 @@ public class Main {
         int c = scanner.nextInt();
         if ((a % 5 != 0) && (b % 5 != 0) && (c % 5 != 0))
             System.out.println("нет значений, кратных 5");
-        if (a % 5 == 0) System.out.print("a=" + a);
-        if (b % 5 == 0) System.out.print(", b=" + b);
-        if (c % 5 == 0) System.out.print(", c=" + c);
+        if (a % 5 == 0)
+            System.out.print("a=" + a + ", ");
+        if (b % 5 == 0)
+            System.out.print("b=" + b + ", ");
+        if (c % 5 == 0)
+            System.out.print("c=" + c);
         System.out.println("\n"+a/b);
         System.out.println(((double) a/(double) b));
         System.out.println((int) Math.ceil((double) a / b));
